07e8314e4f345a1f49df86b756a3e79ff2d203cc
eowre <evanowre12@gmail.com>
2023-03-08 15:26:15 -0500

    Fixing swagger running

--- LifeTrainerApi/Program.cs
+++ LifeTrainerApi/Program.cs
@@ -30,13 +30,21 @@ namespace LifeTrainerApi
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Title = "Minimal API",
+                    Version = "v1"
+                });
+            });
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
+                app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
